3f65036f41f049d870ba75d04893c691f5dcb5ab
Yuvaraj <yuvaraj.a.r@intel.com>
2024-12-18 14:42:08 +0530

    Updated docstring

--- calculator.py
+++ calculator.py
@@ -1,10 +1,14 @@
+# Simple calculator module
 def add(a, b):
+    # Function for Addition
     return a + b
 
 def subtract(a, b):
+    # Function for Substraction
     return a - b
 
 def multiply(a, b):
+    # Function for Multiplication
     return a * b
 
 def divide(a, b):
